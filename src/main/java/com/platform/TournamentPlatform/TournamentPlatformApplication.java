a501788a4b2a2bc4440ecd05a378c7247c42ac7c
Fybea <alexzey.198@gmail.com>
2024-02-15 17:39:38 +0300

    Initial commit

---
+++ src/main/java/com/platform/TournamentPlatform/TournamentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.platform.TournamentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/platform/TournamentPlatform/model/User.java
@@ -0,0 +1,4 @@
+package com.platform.TournamentPlatform.model;
+
+public class User {
+}
